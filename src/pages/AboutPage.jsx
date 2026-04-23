1dd9b90e2999eaa32be760f3bd2831587b1a29b5
Teo <teodora.cristiana.stoicescu@gmail.com>
2026-04-23 22:35:42 +0200

    Initial commit with the pages

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+﻿import { Link } from 'react-router-dom';
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is a Pokédex built with React and Vite</p>
+            <Link to="/">Back to Pokédex</Link>
+        </div>
+    );
+}
---
+++ src/pages/PokedexPage.jsx
@@ -0,0 +1,11 @@
+﻿import { Link } from 'react-router-dom';
+
+export default function PokedexPage() {
+    return (
+        <div>
+            <h1>Pokédex</h1>
+            <p>Pokémon list will go here</p>
+            <Link to="/about">About</Link>
+        </div>
+    );
+}
